802b605b76b32842757555acaae98ea1f584b4df
Karjeje <193631637+Karjeje@users.noreply.github.com>
2025-11-25 01:43:58 +0100

    Set up multiple js and html files

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,42 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-   entry: {
-     app: './src/index.js',
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       template: './src/template.html',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
-   module: {
-     rules: [
-       {
-         test: /\.css$/i,
-         use: ["style-loader", "css-loader"],
-       },
-     ],
+  entry: {
+    home: "./src/home/index.js",
+    page1: "./src/page1/page1.js",
+    page2: "./src/page2/page2.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/home/index.html",
+      filename: "index.html",
+      chunks: ["home"],
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/page1/page1.html",
+      filename: "page1.html",
+      chunks: ["page1"],
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/page2/page2.html",
+      filename: "page2.html",
+      chunks: ["page2"],
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
