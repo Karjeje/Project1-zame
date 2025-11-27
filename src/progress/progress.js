ed33ad26bb2a5e68bb99e67bb8c2483073a88c09
Karjeje <193631637+Karjeje@users.noreply.github.com>
2025-11-27 14:10:26 +0100

    Write functions to group activities  by week

--- src/progress/progress.js
+++ src/progress/progress.js
@@ -9,3 +9,28 @@ function loadActivities() {
 function getTypes(activities) {
   return [...new Set(activities.map((a) => a.type))];
 }
+
+//Group activities into weeks
+function getWeekStart(date) {
+  const d = new Date(date);
+  const day = d.getDay();
+  const diff = d.getDate() - day + 1;
+  return new Date(d.setDate(diff));
+}
+
+function groupByWeek(activities, type) {
+  const filtered = activities.filter((a) => a.type === type);
+
+  const weeks = {};
+
+  filtered.forEach((a) => {
+    const weekStart = getWeekStart(a.date);
+    const key = weekStart.toISOString().split("T")[0];
+
+    if (!weeks[key]) weeks[key] = 0;
+
+    weeks[key] += a.minutes;
+  });
+
+  return weeks;
+}
